298c2ab18a2a7e90c41199f42041366f81b04124
nimisha <guptanimisha90@gmail.com>
2024-02-15 10:39:22 +0530

    delete linkj pages in routes delete dropdown logic

--- quasar-project/src/components/modules/menu/SellerMenu.vue
+++ quasar-project/src/components/modules/menu/SellerMenu.vue
@@ -4,6 +4,10 @@
             <q-item-section avatar><q-icon :name="item.icon"></q-icon></q-item-section>
             <q-item-section >{{ item.label }}</q-item-section>
         </q-item>
+        <select style="width:150px" id="slct1">
+            <option>Listing</option></select>
+            <br>
+           <select style="width:150px;" id="slct2"></select>
     </q-list>
 </template>
 <script>
@@ -11,16 +15,17 @@ export default {
     data(){
         return{
             menu:[
-                {label: 'Account', icon:'person', url:'/account'},
-                {label: 'Account', icon:'person', url:'/account'},
-                {label: 'Account', icon:'person', url:'/account'},
-                {label: 'Account', icon:'person', url:'/account'},
-                {label: 'Account', icon:'person', url:'/account'},
-                {label: 'Account', icon:'person', url:'/account'},
-                {label: 'Account', icon:'person', url:'/account'},
-                {label: 'Account', icon:'person', url:'/account'}
+                {label: 'Home', icon:'person', url:'/account'},
+                {label: 'Listings', icon:'person', url:'/account'},
+                {label: 'Inventory', icon:'person', url:'/account'},
+                {label: 'Orders', icon:'person', url:'/account'},
+                {label: 'Payments', icon:'person', url:'/account'},
+                {label: 'Reports', icon:'person', url:'/account'},
+                {label: 'More', icon:'person', url:'/account'},
+                {label: 'Help', icon:'person', url:'/account'}
             ]
         }
     }
+
 }
 </script>
--- quasar-project/src/router/routes.js
+++ quasar-project/src/router/routes.js
@@ -5,21 +5,7 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') },
-      { path: 'customer', component: () => import('pages/customer.vue')},
-      { path: 'loginPage', component: () => import('pages/loginPage.vue')},
-      { path: 'signup', component: () => import('pages/signup.vue')},
-      { path: 'address', component: () => import('pages/address.vue')},
-      { path: 'order', component: () => import('pages/order.vue')},
-      { path: 'orderDetails', component: () => import('pages/orderDetails.vue')},
-      { path: 'payment', component: () => import('pages/payment.vue')},
-      { path: 'productCatogory', component: () => import('pages/productCategory.vue')},
-      { path: 'product', component: () => import('pages/product.vue')},
-      // { path: 'seller', component: () => import('pages/seller.vue')},
-      { path: 'sellerProduct', component: () => import('pages/sellerProduct.vue')},
-      { path: 'sellerProductPricing', component: () => import('pages/sellerProductPricing.vue')},
-      { path: 'shoppingCart', component: () => import('pages/shoppingCart.vue')},
-      { path: 'shippingInformation', component: () => import('pages/shippingInformation.vue')},
-    ]
+     ]
   },
 
   {
