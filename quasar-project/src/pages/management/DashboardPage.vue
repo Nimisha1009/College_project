c0d47a1255a88a53d2e23ada489c29421e70c42f
nimisha <guptanimisha90@gmail.com>
2024-03-07 14:16:32 +0530

    managemnt

--- quasar-project/src/pages/management/DashboardPage.vue
+++ quasar-project/src/pages/management/DashboardPage.vue
@@ -1,6 +1,9 @@
 <template>
     <q-page>
-        Management Dashboard1
+        <div class="text-h5 q-ma-md">Dashboard</div>
+        <div class="row q-gutter-md item-center justify-center">
+            <q-card v-for="i in 4" style="width:320px; height:150px"></q-card>
+        </div>
     </q-page>
 </template>
 
