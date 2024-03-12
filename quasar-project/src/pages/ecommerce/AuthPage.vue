d7bead670c347ff6721236ede8a1b1c29341f89a
nimisha <guptanimisha90@gmail.com>
2024-03-12 10:22:53 +0530

    authpage

--- quasar-project/src/pages/ecommerce/AuthPage.vue
+++ quasar-project/src/pages/ecommerce/AuthPage.vue
@@ -1,7 +1,6 @@
 <template>
   <q-page class="flex flex-center">
-    <q-img
-          src="../../assets/pic13.jpg" sizes="(max-width: 320px) 280px"/>
+    
     <q-card class="q-pa-md" style="width:360px">
       <RouterView />
     </q-card>
