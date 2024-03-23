4217ba62111a2a545cb1d0b41156727fc62e6da9
Rakesh Roshan <ricky.it.expert@gmail.com>
2024-03-23 10:29:36 +0530

    Menu Linked to Backend

--- quasar-project/src/components/commons/TopMenu.vue
+++ quasar-project/src/components/commons/TopMenu.vue
@@ -1,8 +1,8 @@
 <template>
-<div class="row m">
+  <div class="row m">
     <template v-for="item of menu">
-      <q-btn :outline="variant === 'outlined'" v-if="item.url" :flat="!variant" :label="item.label"
-        :icon="item.icon || 'perm_identity'" :to="item.url"></q-btn>
+      <q-btn :outline="variant === 'outlined'" v-if="item.url && (item?.subMenu?.length || 0) === 0" :flat="!variant"
+        :label="item.label" :icon="item.icon || 'perm_identity'" :to="item.url"></q-btn>
       <q-btn-dropdown v-if="item.subMenu" :outline="variant === 'outlined'" :to="item.url" :flat="!variant"
         :label="item.label">
         <q-list clickable separator>
--- quasar-project/src/components/modules/menu/EcommerceMenu.vue
+++ quasar-project/src/components/modules/menu/EcommerceMenu.vue
@@ -1,7 +1,7 @@
 <template>
 
   <div class="row">
-    <Menu :menu="menu.ecommerce"></Menu>
+    <Menu :menu="menu"></Menu>
   </div>
 
 
@@ -10,14 +10,52 @@
 
 <script>
 import Menu from 'components/commons/TopMenu.vue'
-import menu from 'src/data/menu.js'
-console.log(menu)
+
 export default {
   components: { Menu },
   data () {
     return {
-      menu
+      categories: []
     }
+  },
+  computed: {
+    menu () {
+      return this.categories.map(category => {
+        let menuItem = {}
+        menuItem.label = category.name
+        menuItem.url = category.sub_categories.length > 0 ? '/' : ''
+        menuItem.icon = ''
+        if (category.sub_categories.length > 0) {
+          menuItem.subMenu = category.sub_categories.map(sc => {
+            let subMenuItem = {}
+            subMenuItem.label = sc.name
+            subMenuItem.url = '/'
+            subMenuItem.icon = ''
+            return subMenuItem
+          })
+        }
+
+        return menuItem
+      })
+    }
+  },
+  methods: {
+    async fetchCategories () {
+      let httpClient = await this.$api.get('/items/categories', {
+        params: {
+          fields: ['*', 'sub_categories.*'],
+          filter: {
+            parent_category_id: {
+              _null: true
+            }
+          }
+        }
+      })
+      this.categories = httpClient.data.data
+    }
+  },
+  created () {
+    this.fetchCategories()
   }
 
 }
